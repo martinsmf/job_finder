bc5de1472ec1b205289b218fbbc86c914b61d72f
Matheus Martins <matheus.martins@rarolabs.com>
2021-09-21 18:09:17 -0300

    search concluded

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const app = express();
 const bodyParser = require('body-parser');
 const db = require('./db/connection');
 const Job = require('./models/Job')
-const path = require('path')
+const path = require('path');
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 
 const PORT = 3000;
 
@@ -35,19 +37,37 @@ db
 
 // routes
 app.get('/', (req, res) => {
-    Job.findAll({
-        order: [
-            ['createdAt', 'DESC']
-        ]
-    })
-        .then(jobs => {
-            res.render('index', {
-                jobs
-            });
+
+    let search = req.query.job;
+    let query = `%${search}%`;
+
+    if (!search) {
+        Job.findAll({
+            order: [
+                ['createdAt', 'DESC']
+            ]
         })
-        .catch(err => {
-            console.log("O correu um erro:" + err)
-        });
+            .then(jobs => {
+                res.render('index', {
+                    jobs
+                });
+            })
+            .catch(err => {
+                console.log("O correu um erro:" + err)
+            });
+    } else {
+        Job.findAll({
+            where: { title: { [Op.like]: query } },
+            order: [
+                ['createdAt', 'DESC']
+            ]
+        })
+            .then(jobs => {
+                res.render('index', {
+                    jobs, search
+                });
+            });
+    }
 });
 
 // jobs routes
